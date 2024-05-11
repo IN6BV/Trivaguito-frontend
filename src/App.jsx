7d2978230bcfd51fbc090d5f488131692152045d
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-05-10 22:53:53 -0600

    se trabajo en el dashboard page y en el auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useRoutes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import routes from "./routes.jsx";
 
-
-function App() {
-
-}
-
-
+export const App = () => {
+  let element = useRoutes(routes);
+  return (
+    <>
+      {element}
+      <Toaster position="top-center" reverseOrder={false} />
+    </>
+  );
+};
---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import { DashboardPage } from "./pages/dashboard/DashboardPage";
+import { AuthPage } from "./pages/auth/AuthPage";
+
+const routes = [
+    {path: '/auth', element: <AuthPage/>},
+    {path: '/*', element: <DashboardPage/>}
+]
+
+export default routes
