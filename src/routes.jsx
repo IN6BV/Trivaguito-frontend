d01c041ff28f4acb3cd42f15af79df8a80df63d4
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-05-18 14:34:20 -0600

    Se agrego las rutas al routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,14 @@
 import { DashboardPage } from "./pages/dashboard/DashboardPage";
 import { AuthPage } from "./pages/auth/AuthPage";
+import { DashboardHotel } from "./pages/dashboardHotel";
+import { DashboardPlataform } from "./pages/dashboardPlataform";
 
 const routes = [
     {path: '/auth', element: <AuthPage/>},
-    {path: '/*', element: <DashboardPage/>}
+    {path: '/*', element: <DashboardPage/>},
+    {path: '/plataformManager', element: <DashboardPlataform/>},
+    {path: '/hotelManager', element: <DashboardHotel/>}
+
 ]
 
 export default routes
