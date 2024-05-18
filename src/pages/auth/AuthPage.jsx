f64a2b4cbdcadc1ea0d97c10acf0bd2d7a120110
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-05-18 08:49:16 -0600

    se hizo el diseño del login y se agregaron validaciones

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -1,7 +1,22 @@
-
+import logo from '../../assets/img/fav.png'
+import './authPage.css'
 
 export const AuthPage = () => {
   return (
-    <div>AuthPage</div>
-  )
+    <div className="container">
+    <div className="logo">
+        <img src={logo} alt="Logo" className='logo'/>
+    </div>
+    <h1>Welcome</h1>
+    <form >
+        <input type="email" placeholder="Email"  />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log in</button>
+    </form>
+    <div className="link">
+        <a href="/register" className='registro'>¡Regístrate ahora!</a>
+    </div>
+</div>
+);
+
 }
