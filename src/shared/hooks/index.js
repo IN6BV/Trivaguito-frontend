f729036b3bb5c6b06c330401e71e0695a4fd3707
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-05-20 16:49:17 -0600

    se hizo el componenete de my account

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -3,5 +3,6 @@ export {useRegisterH} from "./useRegisterH"
 export { useLogin } from "./useLogin";
 export {useRegister} from './useRegister'
 export {logout} from './useLogout'
+export {useUserById} from './useUserById'
 
 export {useRegisterHabitacion} from "./useRegistroHabitaciones"
